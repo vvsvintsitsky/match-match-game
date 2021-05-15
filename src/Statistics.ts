e89891460ad21902716d99d5ec7cce53843f2d93
Uladzislau Svintsitski <v.v.svintsitsky@gmail.com>
2021-05-15 11:19:52 +0300

    refactor: add explicit acces modifier

--- src/Statistics.ts
+++ src/Statistics.ts
@@ -3,14 +3,14 @@ import { GameStatistics } from "./types";
 export class Statistics implements GameStatistics {
     private correntGuesses = 0;
 
-    addCorrectGuess(): void {
+    public addCorrectGuess(): void {
         this.correntGuesses++;
     }
     
-    addWrongGuess(): void {
+    public addWrongGuess(): void {
     }
     
-    calculateScore(timeSpentMilliseconds: number): number {
+    public calculateScore(timeSpentMilliseconds: number): number {
         const score = Math.round(this.correntGuesses * 100 - timeSpentMilliseconds / 100);
 
         return score < 0 ? 0 : score;
